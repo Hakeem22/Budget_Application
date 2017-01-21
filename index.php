9c4075f9783404becacbae2fa77fd8baa0af0fca
Resolute96 <resolute_@hotmail.co.uk>
2017-01-21 15:47:43 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST["username"])) {
+    echo "Welcome back to the site " . $_POST['username'];
+}
+
+?>
+
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Homepage</title>
+</head>
+
+</html>
---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Registration Page</title>
+</head>
+
+<body>
+<center>
+    <form action="index.php" method="POST">
+        Username:<br>
+        <input type="text" name="username"><br>
+        Password:<br>
+        <input type="password" name="password"><br>
+        Email:<br>
+        <input type="text" name="email"><br>
+        <input type="submit" value="Submit">
+    </form>
+</center>
+<div id="text"></div>
+</body>
+
+</html>
