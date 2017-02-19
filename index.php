9b19cde0a476f4bb9cb9d96b1cf556395ad744b6
Resolute96 <resolute_@hotmail.co.uk>
2017-02-19 22:09:33 +0000

    Modifications to the registration page and made a start towards the login page as well. Deleted the login.php file as its unnecessary to have as the index.php will represet the same content that would have been shown on the login.php file.

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+include ('includes/dbconfig.php');
+session_start();
+
+$errors = array();
+
+if (isset($_POST['submit']) || isset($_SESSION['login_user'])) {
+    echo "testing";
+}
 
 ?>
 
@@ -8,5 +16,21 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Homepage</title>
 </head>
+
+<body>
+
+<div id="login_box">
+    <form action="#" method="post">
+        Username:<br>
+        <input type="text" name="username" id="username"><br>
+        Password:<br>
+        <input type="password" name="password" id="password"><br>
+        <input type="submit" name="submit" value="Login">
+        <input type="submit" name="passwordreset" value="Forgot Password?">
+    </form>
+</div>
+
+
+</body>
 
 </html>
--- register.php
+++ register.php
@@ -5,33 +5,39 @@ session_start();
 $errors = array();
 
 if (isset($_POST['submit'])) {
+
     if (empty($_POST['user'])) {
         $errors[] = 'Please insert a username into the input field.';
-    }
-    if (empty($_POST['pass'])) {
+    } else if (empty($_POST['pass'])) {
         $errors[] = 'Please enter a password into the input field.';
-    }
-    if (empty($_POST['email'])) {
+    } else if (empty($_POST['email'])) {
         $errors[] = 'Please enter a valid email address into the input field.';
     }
 
     if (empty($errors)) {
-        $username = mysqli_real_escape_string($conn, $_POST['user']);//create it so it checks emails as well
+        $username = mysqli_real_escape_string($conn, $_POST['user']);
         $password = mysqli_real_escape_string($conn, $_POST['pass']);
         $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $sql = "SELECT * FROM users.users WHERE Username = '$username'";
+
+        $sql = "SELECT * FROM users.users WHERE username = '$username'";
         $result = mysqli_query($conn, $sql);
         $count = mysqli_num_rows($result);
 
+        $sqlemail = "SELECT * FROM users.users WHERE email = '$email'";
+        $result2 = mysqli_query($conn, $sqlemail);
+        $counter = mysqli_num_rows($result);
+
         if ($count > 0) {
             $errors[] = "The credentials you have entered have been used already. Please try with another username.";
+        } else if ($counter > 0) {
+            $errors[] = "The credentials you have entered have been used already. Please try with another email.";
         } else {
             $insert = "INSERT INTO users.users (Username, Password, EmailAddress) VALUES ('$username', '$password', '$email')";
+            $_SESSION['login_user'] = $username;
             mysqli_query($conn, $insert);
-            $errors[] = "The credentials have not been used previously therefore your account has been created try to login <a href='login.php'>here</a>";//register user here
+            $errors[] = "The credentials have not been used previously therefore your account has been created try to login <a href='index.php'>here</a>";
         }
     }
-
 }
 
 mysqli_close($conn);
@@ -46,6 +52,7 @@ mysqli_close($conn);
 </head>
 
 <body>
+<div id="login_box">
     <form action="" method="post">
         Username:<br>
         <input type="text" id="user" name="user" placeholder="Enter your username"><br>
@@ -56,13 +63,16 @@ mysqli_close($conn);
         <input type="submit" name="submit" value="Register">
     </form>
 
-    <div id="message">
-        <?php
-        foreach ($errors as $key => $values) {
-            echo '<br>' . $values;
-        }
-        ?>
-    </div>
+</div>
+
+<div id="message">
+    <?php
+    foreach ($errors as $key => $values) {
+        echo '<br>' . $values;
+    }
+    ?>
+</div>
+
 </body>
 
 </html>
