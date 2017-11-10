9a9cd87761e16df2fc58f4d3ac4e46bc431aaa9a
Resolute96 <resolute_@hotmail.co.uk>
2017-11-10 13:24:49 +0000

    I have added a search box for the end user to search for content that is on our website.

--- index.php
+++ index.php
@@ -34,32 +34,57 @@ if (isset($_POST['submit'])) {
 
     <div id="login_box">
         <center><form action="#" method="post">
-            <?php
-            if (!isset($_SESSION['login_user'])) {
-                ?>
-                <label>Username:</label><br>
-                <input type="text" name="username" id="username"><br>
-                <label>Password:</label><br>
-                <input type="password" name="password" id="password"><br>
-                <input type="submit" name="submit" value="Login">
-                <input type="submit" name="passwordreset" value="Forgotten Password" onclick="window.open('./recovery')"><br><br>
                 <?php
-                echo $errors;
-            } else {
+                if (!isset($_SESSION['login_user'])) {
+                    ?>
+                    <label>Username:</label><br>
+                    <input type="text" name="username" id="username"><br>
+                    <label>Password:</label><br>
+                    <input type="password" name="password" id="password"><br>
+                    <input type="submit" name="submit" value="Login">
+                    <input type="submit" name="passwordreset" value="Forgotten Password" onclick="window.open('./recovery')"><br><br>
+                    <?php
+                    echo $errors;
+                } else {
                 ?>
                 <h1>Heading</h1>
-                <p class="test">Welcome back <b> <?php echo ucfirst($_SESSION['login_user'])  ?></b>, we have been waiting for your return.</p>
-                <input type="search"><br>
-                <input type="submit" value="Submit">
-                <?php
-            }
-            ?>
+                <p class="test">Welcome back <b> <?php echo ucfirst($_SESSION['login_user'])  ?></b>, we have been waiting for your return. Please use the search bar below.</p>
+
+
+                <input type="text" id="myInput" onkeyup="myFunction()" title="Type in a name">
+
+                <ul id="myUL">
+                    <li><a href="#">Test</a></li>
+                    <li><a href="#">123</a></li>
+                    <li><a href="#">Hakeem</a></li>
+                    <?php
+                    }
+                    ?>
             </form></center>
 
     </div>
 
 </div>
 
+
+<script>
+    function myFunction() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementsByTagName("li");
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+
+            }
+        }
+    }
+</script>
 </body>
 
 </html>
