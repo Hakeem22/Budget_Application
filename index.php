73817a6972a8baa2280ec74f640e20128ebd5c8d
Resolute <resolute_@hotmail.co.uk>
2019-01-07 20:40:39 +0000

    I have added in Bootstrap to customise the front-end of the website.

--- index.php
+++ index.php
@@ -24,6 +24,9 @@ if (isset($_POST['submit'])) {
 <html>
 
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Homepage</title>
 </head>
@@ -32,37 +35,73 @@ if (isset($_POST['submit'])) {
 
 <div id="container">
 
-    <div id="login_box">
-        <center><form action="#" method="post">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Customer Login</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+
+                <li><a href="contact.php">Contact</a></li>
+
                 <?php
-                if (!isset($_SESSION['login_user'])) {
-                    ?>
-                    <label>Username:</label><br>
-                    <input type="text" name="username" id="username"><br>
-                    <label>Password:</label><br>
-                    <input type="password" name="password" id="password"><br>
-                    <input type="submit" name="submit" value="Login">
-                    <input type="submit" name="passwordreset" value="Forgotten Password" onclick="window.open('./recovery')"><br><br>
-                    <?php
-                    echo $errors;
-                } else {
-                ?>
-                <h1>Heading</h1>
-                <p class="test">Welcome back <b> <?php echo ucfirst($_SESSION['login_user'])  ?></b>, we have been waiting for your return. Please use the search bar below.</p>
+
+                if (isset($_SESSION['login_user'])) {?>
+
+                <li><a href="logout.php">Sign Out</a></li>
+
+                <?php
+                }?>
+
+            </ul>
+        </div>
+    </nav>
+
+    <form action="#" method="post">
+        <?php
+        if (!isset($_SESSION['login_user'])) {
+            ?>
+
+            <div class="form-group" align="center">
+                <label for="inlineFormInputGroup">Username:</label>
+                <input type="text" class="form-control"" name="username" id="username" style="width: 250px">
+            </div>
+
+            <div class="form-group" align="center">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" name="password" id="password" style="width: 250px">
+            </div>
+
+            <div class="form-check" align="center">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+            </div>
+
+            <div class="buttons" align="center">
+                <input type="submit" class="btn btn-primary" name="submit" value="Login">
+                <input type="submit" class="btn btn-primary" name="passwordreset" value="Forgotten Password" onclick="window.open('./recovery')"><br><br>
+            </div>
+
+            <?php
+            echo $errors;
+        } else {
+        ?>
+        <h1>Heading</h1>
+        <p class="test">Welcome back <b> <?php echo ucfirst($_SESSION['login_user'])  ?></b>, we have been waiting for your return. Please use the search bar below.</p>
 
 
-                <input type="text" id="myInput" onkeyup="myFunction()" title="Type in a name">
+        <input type="text" id="myInput" onkeyup="myFunction()" title="Type in a name">
 
-                <ul id="myUL">
-                    <li><a href="#">Test</a></li>
-                    <li><a href="#">123</a></li>
-                    <li><a href="#">Hakeem</a></li>
-                    <?php
-                    }
-                    ?>
-            </form></center>
+        <ul id="myUL">
+            <li><a href="#">Test</a></li>
+            <li><a href="#">123</a></li>
+            <li><a href="#">Hakeem</a></li>
+            <?php
+            }
+            ?>
+    </form>
 
-    </div>
 
 </div>
 
@@ -85,6 +124,7 @@ if (isset($_POST['submit'])) {
         }
     }
 </script>
+
 </body>
 
 </html>
